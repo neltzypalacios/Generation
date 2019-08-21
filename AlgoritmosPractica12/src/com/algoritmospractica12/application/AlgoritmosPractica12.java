55e9edc5695a971e42bea8ed25c2a6b9bc9eadcf
nehita <n3hita@gmail.com>
2019-08-21 12:37:39 -0500

    Algorithms

---
+++ AlgoritmosPractica12/src/com/algoritmospractica12/application/AlgoritmosPractica12.java
@@ -0,0 +1,16 @@
+package com.algoritmospractica12.application;
+
+
+public class AlgoritmosPractica12 {
+
+	public static void main(String[] args) {
+		Mensajes objMensaje1 = new Mensajes();
+		Mensajes objMensaje2 = new Mensajes();
+		
+		System.out.println(objMensaje1.diFriendzone());
+		System.out.println(objMensaje2.diHola());
+		
+		
+	}
+
+}
---
+++ AlgoritmosPractica12/src/com/algoritmospractica12/application/Mensajes.java
@@ -0,0 +1,24 @@
+package com.algoritmospractica12.application;
+
+public class Mensajes {
+	
+	private String miMensaje;
+
+	public String diHola()
+	{
+		return "Hola Tu";
+	}
+	public String diFriendzone()
+	{
+		return "Solo amigos";
+	}
+	public String getMiMensaje()
+	{
+		return miMensaje;
+	}
+	public void setMiMensaje(String miMensaje)
+	{
+		this.miMensaje = miMensaje;
+	}
+	
+}
